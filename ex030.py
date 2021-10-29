e205e4d8e24bae5050c87f7731a12ad92cb5641e
azindyr <93406582+azindyr@users.noreply.github.com>
2021-10-29 17:49:17 -0300

    Add files via upload

---
+++ ex030.py
@@ -0,0 +1,10 @@
+from time import sleep
+print('=' * 15, 'Exercício 30', '=' * 15)
+print('Analisador de números.')
+n = int(input('Digite um número para saber se ele é PAR ou ÍMPAR: '))
+if n % 2 == 0 and n != 0:
+    print('O número {} é PAR.'.format(n))
+elif n == 0:
+    print('O número {} é Neutro.'.format(n))
+else:
+    print('O número {} é ÍMPAR.'.format(n))
