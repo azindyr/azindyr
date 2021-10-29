0736b7d3876ed67ee9a5eb0ca62eb371effba977
azindyr <93406582+azindyr@users.noreply.github.com>
2021-10-29 17:25:05 -0300

    Update ex021.py

--- ex021.py
+++ ex021.py
@@ -1,12 +1,11 @@
 import pygame
 print('=' * 15, 'Exercício 21', '=' * 15)
-print('"Ah como todas as mulheres!"-Mestre Esqueleto')
-pygame.init() #inicia o pygame
-pygame.mixer.init() #inicia o tocador de sons do pygame
-pygame.mixer.music.load('esqueleto.mp3') #importa o som para o pygame
-pygame.mixer.music.play() #toca o som
+pygame.init() 
+pygame.mixer.init() 
+pygame.mixer.music.load('esqueleto.mp3') 
+pygame.mixer.music.play() 
 input()
-pygame.event.wait() #espera o som terminar
+pygame.event.wait() 
 
 
 
