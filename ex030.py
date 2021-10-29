26ad933d33dd5fb1a676490b6e709e5edbf40028
azindyr <93406582+azindyr@users.noreply.github.com>
2021-10-29 17:50:08 -0300

    Update ex030.py

--- ex030.py
+++ ex030.py
@@ -2,6 +2,8 @@ from time import sleep
 print('=' * 15, 'Exercício 30', '=' * 15)
 print('Analisador de números.')
 n = int(input('Digite um número para saber se ele é PAR ou ÍMPAR: '))
+print('Analisando resultado...')
+sleep(1)
 if n % 2 == 0 and n != 0:
     print('O número {} é PAR.'.format(n))
 elif n == 0:
